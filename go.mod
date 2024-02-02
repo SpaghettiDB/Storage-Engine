6976e6780df8195d1f94f786b0cb2328552eaadc
alimorgaan <alimorgaan@gmail.com>
2024-02-02 23:40:22 +0200

    add btree

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module KinanaDB/database
 
 go 1.21.6
+
+require github.com/krasun/fbptree v0.0.0-20220109123118-8003ab1ef37b // indirect
--- heapmanager/main.go
+++ heapmanager/main.go
@@ -1,11 +1,11 @@
 /*
 this package is a heap manager that will be used to manage the heap of the database.
 
-
-
-
 */
 
 package heapmanager
 
 // function to create a new heap
+func CreateHeap(name string) {
+
+}
