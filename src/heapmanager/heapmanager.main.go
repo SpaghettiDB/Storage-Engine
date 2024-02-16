bb998c51789a1fc8c5766d75522979fefb89a04a
alimorgaan <alimorgaan@gmail.com>
2024-02-16 18:00:41 +0200

    fix bug for heapmanager

--- src/heapmanager/heapmanager.main.go
+++ src/heapmanager/heapmanager.main.go
@@ -136,6 +136,24 @@ func AddRowToHeap(name string, row []byte) {
 		//overWrite the page to the file
 		overWritePageToHeap(file, int(pageCount-1), page)
 
+		//update the heap header with the new rowCount
+		//read the header in byte slice
+		header = make([]byte, heapHeaderSize)
+		if _, err := file.ReadAt(header, 0); err != nil {
+			return
+		}
+
+		//parse the header
+		_, rowCount := parseHeapHeader(header)
+
+		//update the rowCount
+		rowCount++
+		binary.BigEndian.PutUint32(header[4:8], rowCount)
+
+		//write the header to the file
+		file.WriteAt(header, 0)
+		file.Sync()
+
 	} else {
 
 		//if the free space available is not enough to add the row then add new page
@@ -149,24 +167,6 @@ func AddRowToHeap(name string, row []byte) {
 		//call the function recursively to add the row to the new page
 		AddRowToHeap(name, row)
 	}
-
-	//update the heap header with the new rowCount
-	//read the header in byte slice
-	header = make([]byte, heapHeaderSize)
-	if _, err := file.ReadAt(header, 0); err != nil {
-		return
-	}
-
-	//parse the header
-	_, rowCount := parseHeapHeader(header)
-
-	//update the rowCount
-	rowCount++
-	binary.BigEndian.PutUint32(header[4:8], rowCount)
-
-	//write the header to the file
-	file.WriteAt(header, 0)
-	file.Sync()
 
 }
 
