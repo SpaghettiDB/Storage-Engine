f52965cd201d724c8bc422aa5508ca59293a74b7
alimorgaan <alimorgaan@gmail.com>
2024-04-16 23:03:47 +0200

    shcemamanager init

---
+++ src/schemamanager/schemamanager.main.go
@@ -0,0 +1,24 @@
+//this is schemamanager package main file this module is responsible
+//for storing data about the schema of the database like tables cols and indexes etc
+//for sake of simplicity we will use json file to store the schema data
+
+package schemamanager
+
+
+
+func getTables () []string {
+	//read from schema file
+	return nil 
+}
+
+func getColumns (table string) []string {
+	//read from schema file
+	return nil 
+}
+
+func getIndexes (table string) []string {
+	//read from schema file
+	return nil 
+}
+
+
